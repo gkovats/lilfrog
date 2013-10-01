68ce217fb6c7aba5a7de7713cc1e4985ffa86d79
George Kovats <gkovats@webmd.net>
2013-10-01 04:07:14 -0400

    Restructuring.

--- index.php
+++ index.php
@@ -2,22 +2,17 @@
 
   define('ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-  $f3=require('lib/base.php');
+  $f3=require('lib/f3/base.php');
   $f3->config('config.ini');
   $f3->set('AUTOLOAD','classes/');
 
-  // F3 help text
-  $f3->route('GET /userref',
-    function() {
-      echo View::instance()->render('userref.htm');
-    }
-  );
+	// $site = new controllers\Site();
+
 
   // F3 help text
-  $f3->route('GET /',
-    function() {
-      echo View::instance()->render('home.html');
-    }
-  );
+  // $f3->route('GET /userref', function() { echo View::instance()->render('userref.htm'); } );
+
+  // F3 help text
+  $f3->route('GET /', 'controllers\Site->home' );
 
   $f3->run();
